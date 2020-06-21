cf6ef94f7a0ad0a584372cf48f4add5e84aded91
Gean Lucas <gean.lfpsa@gmail.com>
2020-06-20 23:12:56 -0300

    propriedade techs sendo adicionada como array

--- src/app.js
+++ src/app.js
@@ -24,11 +24,13 @@ app.get("/repositories", (request, response) => {
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body;
 
+  const techsSplitted = techs.split(", ");
+  
   const repository = {
     id: uuid(),
     title, 
     url, 
-    techs,
+    techs: techsSplitted,
     likes: 0
   }
 
